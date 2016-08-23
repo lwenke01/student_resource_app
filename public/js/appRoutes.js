21568d423efe531a9b08bac041792763353f7eaf
lwenke01 <lwenke@gmail.com>
2016-08-22 18:10:10 -0700

    updates

--- public/js/appRoutes.js
+++ public/js/appRoutes.js
@@ -5,20 +5,12 @@ angular.module('appRoutes', []).config(['$routeProvider', '$locationProvider', f
 
   $routeProvider
 
-  // home page
 
-
-  // blogss page that will use the BlogController
 
   .when('/', {
     templateUrl: 'views/home.html',
     controller: 'BlogController'
   });
-
-  //  .when('/:_id', {
-  //        templateUrl: 'views/blog-id.html',
-  //        controller: 'IdController'
-  //      });
 
   $locationProvider.html5Mode(true);
 
